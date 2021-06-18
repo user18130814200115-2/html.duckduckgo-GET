f3f48c13bc5b6144ba0b12bf3d773eea7c8e0977
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-06-18 16:09:53 +0200

    Update duckduckgo.php

--- duckduckgo.php
+++ duckduckgo.php
@@ -1,11 +1,13 @@
 <?php
-$url= "https://html.duckduckgo.com/html/?q=$_GET[q]";
- 
+$url= "https://html.duckduckgo.com/html/?$_SERVER[QUERY_STRING]";   
+
 $lines_array=file($url);
 $lines_string=implode('',$lines_array);
- 
- 
+
+
 $pattern = '/method=\"post\"/i';
 $pattern2 = '/action=\"\/html\/\"/i';
-echo preg_replace($pattern, 'method=get', preg_replace($pattern2, '', $lines_string));
+$lines_string=preg_replace($pattern, 'method=get', preg_replace($pattern2, '', $lines_string));
+   
+echo preg_replace('/\/\/.*uddg=/i', ' ', urldecode("$lines_string"))                     
 ?>
