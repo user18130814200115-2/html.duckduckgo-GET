851e25c54f396261f5a2f0aad7e9eb67c14d3467
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-06-18 22:05:26 +0200

    Update duckduckgo.php

--- duckduckgo.php
+++ duckduckgo.php
@@ -1,13 +1,20 @@
 <?php
-$url= "https://html.duckduckgo.com/html/?$_SERVER[QUERY_STRING]";   
+# Set url including php GET data accoring to our current url
+$url= "https://html.duckduckgo.com/html/?$_SERVER[QUERY_STRING]";
 
+# Get data from duckduckgo
 $lines_array=file($url);
 $lines_string=implode('',$lines_array);
 
-
+# replace method POST by GET
 $pattern = '/method=\"post\"/i';
 $pattern2 = '/action=\"\/html\/\"/i';
 $lines_string=preg_replace($pattern, 'method=get', preg_replace($pattern2, '', $lines_string));
-   
-echo preg_replace('/\/\/.*uddg=/i', ' ', urldecode("$lines_string"))                     
+
+# remove duckduckgo proxy
+$lines_string=preg_replace('/"\/\/.*uddg=(.*).rut.*?"/i', "$1", "$lines_string");
+$lines_string=str_replace('%2F&', '', "$lines_string");   
+
+# echo result
+echo urldecode($lines_string);
 ?>
