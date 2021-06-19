8366de33edb1df0b3010263a3941af01b7389b28
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-06-19 12:09:35 +0200

    Update duckduckgo.php

--- duckduckgo.php
+++ duckduckgo.php
@@ -1,22 +1,25 @@
 <?php
-# Set url including php GET data accoring to our current url
+# set url with php GET data
 $url= "https://html.duckduckgo.com/html/?$_SERVER[QUERY_STRING]";
 
-# Get data from duckduckgo
+# get data from duckduckgo
 $lines_array=file($url);
 $lines_string=implode('',$lines_array);
 
-# replace method POST by GET
+# Change method to GET
 $pattern = '/method=\"post\"/i';
 $pattern2 = '/action=\"\/html\/\"/i';
 $lines_string=preg_replace($pattern, 'method=get', preg_replace($pattern2, '', $lines_string));
 
-# remove duckduckgo proxy
-$lines_string=preg_replace('/"\/\/.*uddg=(.*).rut.*?"/i', "$1", "$lines_string");
+# Function to remove last character form regex below
+# For some reason, the regula expression always appends a "&" this is the only way I found to cleanly get rid of it
+function callback($matches) {
+        return mb_substr($matches[1], 0, -1);
+}
+# Regex to remove duckduckgo proxy
+$lines_string=preg_replace_callback('/"\/\/.*uddg=(.*).rut.*?"/i', "callback", "$lines_string");
 $lines_string=urldecode($lines_string);
-$lines_string=str_replace('/&', '/', "$lines_string");
-$lines_string=str_replace('/amp', '/', "$lines_string");
 
-# echo result
-echo urldecode($lines_string);
+# Output HTML data
+echo "$lines_string"
 ?>
