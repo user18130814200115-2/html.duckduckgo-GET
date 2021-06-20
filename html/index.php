92b00ecbe7dc328b156cad87a2475ffaddd85f23
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-06-20 14:34:23 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -11,14 +11,8 @@ $pattern = '/method=\"post\"/i';
 $pattern2 = '/action=\"\/html\/\"/i';
 $lines_string=preg_replace($pattern, 'method=get', preg_replace($pattern2, '', $lines_string));
 
-# Function to remove last character form regex below
-# For some reason, the regula expression always appends a "&" this is the only way I found to cleanly get rid of it
-function callback($matches) {
-        $href=$matches[1];
-        return substr_replace($href, "", -4);
-}
 # Regex to remove duckduckgo proxy
-$lines_string=preg_replace_callback('/"\/\/.*uddg=(.*).rut.*?"/i', "callback", "$lines_string");
+$lines_string=preg_replace('/"\/\/.*uddg=(.*)&amprut.*?"/i', "$1", "$lines_string");
 $lines_string=urldecode($lines_string);
 
 # Output HTML data
