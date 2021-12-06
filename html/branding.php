64f3b7413e0515db78a734cd8b4911e73970f850
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-12-06 21:23:26 +0100

    Create branding.php

---
+++ html/branding.php
@@ -0,0 +1,44 @@
+<?php
+$url= "https://html.duckduckgo.com/html/?$_SERVER[QUERY_STRING]";
+
+if (substr( $_SERVER[QUERY_STRING], 0, 5 ) === "q=%21") {
+	header("Location: $url");
+	die();
+}
+
+$lines_array=file($url);
+$lines_string=implode('',$lines_array);
+
+$pattern1 = '/method=\"post\"/i';
+$pattern2 = '/action=\"\/html\/\"/i';
+$pattern4 = '/\/\/duckduckgo.com\/favicon.ico/i';
+$pattern5 = '/DuckDuckGo/';
+$pattern6 = '/\/\/duckduckgo.com\/about.html/';
+$pattern3 = '/"\/\/.*uddg=(.*)&.*?rut.*?"/i';
+
+function decode($matches) {
+	return urldecode("$matches[1]");
+}
+
+$lines_string=preg_replace($pattern1, "", $lines_string);
+$lines_string=preg_replace($pattern2, "", $lines_string);
+$lines_string=preg_replace($pattern4, "logo.ico", $lines_string);
+$lines_string=preg_replace($pattern5, "WesterDigitalSearch", $lines_string);
+$lines_string=preg_replace($pattern6, "https://github.com/user18130814200115-2/html.duckduckgo-GET", $lines_string);
+$lines_string=preg_replace_callback($pattern3, decode, $lines_string);
+
+echo $lines_string;
+?>
+<style>
+.result--ad {
+	display: none
+}
+.logo_homepage {
+	background: no-repeat center/100% url("logo.svg"),linear-gradient(transparent, transparent) !important;
+	background-size: contain !important;
+}
+.header__logo-wrap {
+	background: no-repeat center/auto 36px url("logo.svg"),linear-gradient(transparent, transparent) !important;
+	background-size: contain !important;
+}
+</style>
